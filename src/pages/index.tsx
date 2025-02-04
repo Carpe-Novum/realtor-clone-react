cfd19d7ba77ee0cdeb9069897cb46d0b6ed420d9
MARTHINUSLAPTOP\mjsch <marthinusschoombie@gmail.com>
2025-02-04 15:09:41 +0200

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Realtor Clone React</title>
+      </Head>
+      
+      <h1>Hello World!</h1>
+    </>
+  );
+}
